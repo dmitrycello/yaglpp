d0e51d033f87a08cbbdc427a83bf8017bd6f9a8c
dmitrycello <156050563+dmitrycello@users.noreply.github.com>
2024-05-11 03:11:01 -0400

    Updated StbImage class
    Re-structured usage of the API. Using only stbi_ functions (single underscore). Except the set of gif loader. Will be maintained regardless of official release

--- include/StbImage.h
+++ include/StbImage.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "glpp.h"
-/*glpp stb_image callback structure*/
+/*glpp stb_image I/O callback structure*/
 typedef struct
 {
 	/*Fills 'data' with 'size' bytes. Returns number of bytes actually read*/
@@ -81,56 +81,48 @@ enum class StbFormat : int
 class StbImage
 {
 private:
-	typedef struct {
-		union
-		{
-			FILE* file;
-			BYTE* buffer;
-		};
-		int len;
-	} _DATA, *_LPDATA;
 	int m_iByte; // Bytes p/channel
 	int m_iComp; // Channels p/pixel
 	int m_iWidth; // Image width
 	int m_iHeight; // Image height
 	void* m_lpPixels; // Pixel data
 
-	void _byte(_LPDATA data);
-	void _c168(void* data);
-	void _c816(void* data);
-	void _cf08(void* data, int format);
-	void _cf16(void* data, int format);
-	void _conv(void* source, int format);
-	void _copy(const StbImage* source);
-	void _file(_LPDATA data, const char* file);
-	void _h2ld(void* data);
-	int  _info(_LPDATA data);
-	void _l2hd(void* data);
-	void _load(_LPDATA data, int format);
-	int  _lgif(_LPDATA data, int format, int** delays);
-	void _rcid(_LPDATA data, int rcid);
-	bool _resz(void* source, int ow, int oh, int rx, int ry, int rw, int rh);
-
 public:
 	/*(1) Constructs an empty StbImage object*/
 	StbImage();
 
-	/*(2) Constructs a copy StbImage object with <convert>*/
-	StbImage(_In_ const StbImage& source)
+	/*(2) Constructs a copy StbImage object with <copy>*/
+	StbImage(const StbImage& source)
 	{
 		m_lpPixels = nullptr;
-		convert(source, StbFormat::Default);
+		copy(source);
 	}
 
-	/*(3) Constructs StbImage object with <load>*/
+	/*(3) Constructs a copy StbImage object with <copySprite>*/
+	StbImage(const StbImage& source, int width, int height, int index)
+	{
+		m_lpPixels = nullptr;
+		copySprite(source, width, height, index);
+	}
+
+	/*(4) Constructs a copy StbImage object with <copyRegion>*/
+	StbImage(const StbImage& source, int rleft, int rtop, int rwidth, int rheight)
+	{
+		m_lpPixels = nullptr;
+		copyRegion(source, rleft, rtop, rwidth, rheight);
+	}
+
+	/*(5) Constructs StbImage object with <load>*/
 	StbImage(int rcid, StbFormat format = StbFormat::Default)
 	{
+		m_lpPixels = nullptr;
 		load(rcid, format);
 	}
 
-	/*(4) Constructs StbImage object with <load>*/
+	/*(6) Constructs StbImage object with <load>*/
 	StbImage(_In_z_ const char* file, StbFormat format = StbFormat::Default)
 	{
+		m_lpPixels = nullptr;
 		load(file, format);
 	}
 
@@ -143,83 +135,37 @@ public:
 		}
 	}
 
-	/*(1) Converts loaded image to the new pixel format. Although it might be necessary, often better to choose the right pixel format at image loading. Direct conversions are 8u-16u, 16u-8u, 8u-32f, 32f-8u. Conversions 16u-32f and 32f-16u are performed in two steps. The 32-bit float format conversion isn't provided by <stb_image>, so it is performed in three steps: downgraded to 8-bit, remapped and then converted back to 32-bit float
-	@param The new pixel format, must differ from the source*/
-	void convert(StbFormat format)
-	{
-		_conv(getPixels(), (int)format);
-	}
-
-	/*(2) Converts the image currently loaded into the source StbImage object to the new pixel format. Although it might be necessary, often better to choose the right pixel format at image loading. Direct conversions are 8u-16u, 16u-8u, 8u-32f, 32f-8u. Conversions 16u-32f and 32f-16u are performed in two steps. The 32-bit float format conversion isn't provided by <stb_image>, so it is performed in three steps: downgraded to 8-bit, remapped and then converted back to 32-bit float
-	@param [in] The source StbImage object 
-	@param The new pixel format, must differ from the source*/
-	void convert(_In_ const StbImage& source, StbFormat format);
-
-	/*(1) Converts loaded 16-bit image to the 8-bit pixel depth*/
-	void convert16To8()
-	{
-		_c168(getPixels());
-	}
-
-	/*(2) Converts 16-bit image currently loaded into the source StbImage object to the 8-bit pixel depth
-	@param [in] The source StbImage object*/
-	void convert16To8(_In_ const StbImage& source);
-
-	/*(1) Converts loaded 8-bit image to the 16-bit pixel depth*/
-	void convert8To16()
-	{
-		_c816(getPixels());
-	}
-
-	/*(2) Converts 8-bit image currently loaded into the source StbImage object to the 16-bit pixel depth
-	@param [in] The source StbImage object*/
-	void convert8To16(_In_ const StbImage& source);
-
-	/*(1) Converts loaded 8-bit image to new channel per pixel format
-	@param The new channel per pixel format, must be non-default and differ from the source*/
-	void convertFormat(StbFormat format)
-	{
-		_cf08(getPixels(), (int)format);
-	}
-
-	/*(2) Converts 8-bit image currently loaded into the source StbImage object to new channel per pixel format
-	@param [in] The source StbImage object
-	@param The new channel per pixel format, must be non-default and differ from the source*/
-	void convertFormat(_In_ const StbImage& source, StbFormat format);
-
-	/*(1) Converts loaded 16-bit image to new channel per pixel format
-	@param The new channel per pixel format, must be non-default and differ from the source*/
-	void convertFormat16(StbFormat format)
-	{
-		_cf16(getPixels(), (int)format);
-	}
-
-	/*(2) Converts 16-bit image currently loaded into the source StbImage object to new channel per pixel format
-	@param [in] The source StbImage object
-	@param The new channel per pixel format, must be non-default and differ from the source*/
-	void convertFormat16(_In_ const StbImage& source, StbFormat format);
-	
 	/*Sets the global flag indicating whether to process iphone images back to canonical format, or pass them through as is
 	@param True to convert BGR to RGB for PNG files, default false*/
 	static void convertIphonePngToRgb(bool convert);
 
-	/*Duplicates the source StbImage object
+	/*Copies the source StbImage object
 	@param [in] The source StbImage object*/
-	void copy(_In_ const StbImage& source);
+	void copy(const StbImage& source);
 
-	/*Duplicates a region of the source StbImage object
+	/*(1) Copies a region of currently loaded image replacing the old image
+	@param The region top-left corner x coordinate on the image
+	@param The region top-left corner y coordinate on the image
+	@param The width of the region
+	@param The height of the region*/
+	void copyRegion(int rleft, int rtop, int rwidth, int rheight)
+	{
+		copyRegion(*this, rleft, rtop, rwidth, rheight);
+	}
+
+	/*(2) Copies a region of the image currently loaded into the source StbImage object
 	@param [in] The source StbImage object
 	@param The region top-left corner x coordinate on the image
 	@param The region top-left corner y coordinate on the image
 	@param The width of the region
 	@param The height of the region*/
-	void copyRegion(_In_ const StbImage& source, int rleft, int rtop, int rwidth, int rheight);
+	void copyRegion(const StbImage& source, int rleft, int rtop, int rwidth, int rheight);
 
 	/*Duplicates one of the inbound sprites in the source StbImage object selected by index value. The sprite's dimentions must by multiple of the image's dimentions, and remain the same within the image
 	@param The width of the sprite
 	@param The height of the sprite
 	@param The index of the sprite*/
-	void copySprite(_In_ const StbImage& source, int width, int height, int index);
+	void copySprite(const StbImage& source, int width, int height, int index);
 
 	/*Creates an empty image pixel data array within StbImage object. The object must be empty, and the pixel format has to be determined, which means no default values such as <Rgb> or <Unsigned8> should be used
 	@param The new image width
@@ -234,52 +180,42 @@ public:
 
 	/*Gets the number of channels per pixel of loaded or queried image. Possible values are: <Grey>, <GreyAlpha>, <Rgb> or <RgbAlpha>
 	@return The number of channels per pixel*/
-	StbFormat getChannels()
+	StbFormat getChannels() const
 	{
 		return (StbFormat)m_iComp;
 	}
 
 	/*Gets the channel depth of loaded or queried image. Possible values are: <Unsigned8>, <Unsigned16> or <Float32>
 	@return The pixel channel depth*/
-	StbFormat getDepth()
+	StbFormat getDepth() const
 	{
 		return (StbFormat)(m_iByte << 4);
 	}
 
 	/*Gets the number of channels per pixel and the channel depth of loaded or queried image
 	@return The pixel format*/
-	StbFormat getFormat()
+	StbFormat getFormat() const
 	{
 		return (StbFormat)((m_iByte << 4) | m_iComp);
 	}
 
 	/*Gets the height of last loaded image. The multi-image height is multiplied by the number of layers
 	@return The image height in pixels*/
-	int getHeight()
+	int getHeight() const
 	{
 		return m_iHeight;
 	}
 
 	/*Gets the pointer of pixel array of last loaded image
 	@return Pointer to the image pixel array*/
-	_Ret_notnull_ void* getPixels();
+	_Ret_notnull_ void* getPixels() const;
 
 	/*Gets the width of last loaded image
 	@return The image width in pixels*/
-	int getWidth()
+	int getWidth() const
 	{
 		return m_iWidth;
 	}
-
-	/*(1) Converts loaded 32-bit float (HDR) image to the 8-bit unsigned (LDR) pixel depth*/
-	void hdrToLdr()
-	{
-		_h2ld(getPixels());
-	}
-
-	/*(2) Converts 32-bit float (HDR) image currently loaded into the source StbImage object to the 8-bit unsigned (LDR) pixel depth
-	@param [in] The source StbImage object*/
-	void hdrToLdr(_In_ const StbImage& source);
 
 	/*Sets the global gamma value used in hdr-to-ldr conversion
 	@param New hdr-to-ldr gamma value, default 1/2.2f*/
@@ -302,22 +238,50 @@ public:
 	@return True if image information loaded successfully, false otherwise*/
 	bool info(_In_z_ const char* file);
 
+	/*(3) Retrives image information using I/O callback while reading from arbitrary sources. Sets width, height and pixel format without image loading
+	@param [in] Points to callback structure
+	@param [in] Points to user-defined data
+	@return True if image information loaded successfully, false otherwise*/
+	bool info(_In_ StbCallbacks const* clbk, _In_ void* user);
+
+	/*(1) Checks if the binary resource is a 16-bit image
+	@param The image binary resource id of <RCDATA> type
+	@return True if binary resource is 16-bit image, false otherwise*/
+	static bool is16bit(int rcid);
+
+	/*(2) Checks if the file is a 16-bit image
+	@param [in] Path to the image file
+	@return True if file is 16-bit image, false otherwise*/
+	static bool is16bit(_In_z_ const char* file);
+
+	/*(3) Checks if the image is 16-bit using I/O callback while reading from arbitrary sources
+	@param [in] Points to callback structure
+	@param [in] Points to user-defined data
+	@return True if file is 16-bit image, false otherwise*/
+	static bool is16bit(_In_ StbCallbacks const* clbk, _In_ void* user);
+
+	/*(1) Checks if the binary resource is a 32-bit float image
+	@param The image binary resource id of <RCDATA> type
+	@return True if binary resource is 32-bit float image, false otherwise*/
+	static bool isHdr(int rcid);
+
+	/*(2) Checks if the file is a 32-bit float image
+	@param [in] Path to the image file
+	@return True if file is 32-bit float image, false otherwise*/
+	static bool isHdr(_In_z_ const char* file);
+
+	/*(3) Checks if the image is 32-bit float using I/O callback while reading from arbitrary sources
+	@param [in] Points to callback structure
+	@param [in] Points to user-defined data
+	@return True if file is 32-bit float image, false otherwise*/
+	static bool isHdr(_In_ StbCallbacks const* clbk, _In_ void* user);
+
 	/*Checks if the StbImage object has loaded image
 	@return True if the image is loaded, false otherwise*/
 	bool isImage()
 	{
 		return m_lpPixels != nullptr;
 	}
-
-	/*(1) Converts loaded 8-bit unsigned (LDR) image to the 32-bit float (HDR) pixel depth*/
-	void ldrToHdr()
-	{
-		_l2hd(getPixels());
-	}
-
-	/*(2) Converts 8-bit unsigned (LDR) image currently loaded into the source StbImage object to the 32-bit float (HDR) pixel depth
-	@param [in] The source StbImage object*/
-	void ldrToHdr(_In_ const StbImage& source);
 
 	/*Sets the global gamma value used in ldr-to-hdr conversion
 	@param New ldr-to-hdr gamma value, default 2.2f*/
@@ -337,6 +301,12 @@ public:
 	@param The storing pixel format, default is the image source pixel format*/
 	void load(_In_z_ const char* file, StbFormat format = StbFormat::Default);
 
+	/*(3) Loads image using I/O callback while reading from arbitrary sources
+	@param [in] Points to callback structure
+	@param [in] Points to user-defined data
+	@param The storing pixel format, default is the image source pixel format*/
+	void load(_In_ StbCallbacks const* clbk, _In_ void* user, StbFormat format = StbFormat::Default);
+
 	/*(1) Loads GIF image binary resource
 	@param The image resource id of <RCDATA> type
 	@param [out] Pointer to the array of image delays
@@ -350,6 +320,14 @@ public:
 	@param The storing pixel format, default is the image source pixel format
 	@return The number of loaded layers*/
 	int loadGif(_In_z_ const char* file, _Outptr_opt_ int** delays, StbFormat format = StbFormat::Default);
+
+	/*(3) Loads GIF image using I/O callback while reading from arbitrary sources
+	@param [in] Points to callback structure
+	@param [in] Points to user-defined data
+	@param [out] Pointer to the array of image delays
+	@param The storing pixel format, default is the image source pixel format
+	@return The number of loaded layers*/
+	int loadGif(_In_ StbCallbacks const* clbk, _In_ void* user, _Outptr_opt_ int** delays, StbFormat format = StbFormat::Default);
 
 	/*(1) Resizes currently loaded image
 	@param The width of a new image
@@ -365,12 +343,12 @@ public:
 	@param The width of a new image
 	@param The height of a new image
 	@return True if success, false otherwise*/
-	bool resize(_In_ const StbImage& source, int width, int height)
+	bool resize(const StbImage& source, int width, int height)
 	{
 		return resizeRegion(source, width, height, 0, 0, source.m_iWidth, source.m_iHeight);
 	}
 
-	/*(1) Resizes a region of currently loaded image
+	/*(1) Resizes a region of currently loaded image replacing the old image
 	@param The width of a new image
 	@param The height of a new image
 	@param The region top-left corner x coordinate on the image
@@ -378,7 +356,10 @@ public:
 	@param The width of the region
 	@param The height of the region
 	@return True if success, false otherwise*/
-	bool resizeRegion(int width, int height, int rleft, int rtop, int rwidth, int rheight);
+	bool resizeRegion(int width, int height, int rleft, int rtop, int rwidth, int rheight)
+	{
+		return resizeRegion(*this, width, height, rleft, rtop, rwidth, rheight);
+	}
 
 	/*(2) Resizes a region of the image currently loaded into the source StbImage object
 	@param [in] The source StbImage object
@@ -389,11 +370,7 @@ public:
 	@param The width of the region
 	@param The height of the region
 	@return True if success, false otherwise*/
-	bool resizeRegion(_In_ const StbImage& source, int width, int height, int rleft, int rtop, int rwidth, int rheight)
-	{
-		_copy(&source);
-		return _resz(source.m_lpPixels, width, height, rleft, rtop, rwidth, rheight);
-	}
+	bool resizeRegion(const StbImage& source, int width, int height, int rleft, int rtop, int rwidth, int rheight);
 
 	/*Sets the global image loading direction flag
 	@param True to set loading direction from bottom to top, default false*/
@@ -402,13 +379,6 @@ public:
 	/*Sets the global image loading alpha unpremultiplication flag
 	@param True to force removing any premultiplied alpha, default false*/
 	static void setUnpremultiplyOnLoad(bool unpremultiply);
-
-	/*Vertically flips loaded image*/
-	void verticalFlip();
-
-	/*Vertically flips layers of loaded GIF image
-	@param The number of loaded layers*/
-	void verticalFlipSlices(int layers);
 
 	/*Sets the global PNG filter mode value. Forces the PNG filter mode if from 0 to 5
 	@param The new filter mode value, default -1*/
@@ -448,7 +418,7 @@ public:
 }; // class StbImage
 
 #ifndef _DEBUG
-_Ret_notnull_ inline void* StbImage::getPixels()
+_Ret_notnull_ inline void* StbImage::getPixels() const
 {
 	return m_lpPixels;
 }
