ea9e9890401105ef2585ee4cb8aed39a1fc0fe21
dmitrycello <156050563+dmitrycello@users.noreply.github.com>
2024-10-29 02:06:02 -0400

    Add files via upload

---
+++ examples/4.1.textures/resource.h
@@ -0,0 +1,4 @@
+// resource.h
+#define IDT_VERT_SHADER		256
+#define IDT_FRAG_SHADER		257
+#define IDI_CONTAINER		258
---
+++ examples/4.5.textures_exercise3/textures_exercise3.cpp
@@ -0,0 +1,77 @@
+// textures_exercise3.cpp
+#include "stdafx.h"
+#include "window.h"
+#include "shader.h"
+#include "resource.h"
+const unsigned int SCR_WIDTH = 800; // settings
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    Window window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL"); // glfw window creation
+    window.makeContextCurrent();
+    Shader ourShader(IDT_VERT_SHADER, IDT_FRAG_SHADER); // we load resources to keep the application portable
+    float vertices[] = { // set up vertex data (and buffer(s)) and configure vertex attributes
+         // positions         // colors            // texture coords (note that we changed them to 'zoom in' on our texture image)
+         0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.55f,  0.55f, // top right
+         0.5f, -0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.55f,  0.45f, // bottom right
+        -0.5f, -0.5f,  0.0f,  0.0f,  0.0f,  1.0f,  0.45f,  0.45f, // bottom left
+        -0.5f,  0.5f,  0.0f,  1.0f,  1.0f,  0.0f,  0.45f,  0.55f  // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    gl::VertexArray VAO;
+    gl::ArrayBuffer VBO;
+    gl::ElementArrayBuffer EBO;
+    VAO.bindVertexArray(); // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s)
+    VBO.bufferData(sizeof(vertices), vertices, gl::BufferUsage::StaticDraw);
+    EBO.bufferData(sizeof(indices), indices, gl::BufferUsage::StaticDraw);
+
+    gl::VertexAttrib aPos(0), aColor(1), aTexCoord(2); // or use direct attribute location assignment
+    aPos.vertexAttribPointer(gl::VertexAttribSize::Triple, gl::VertexAttribType::Float, GL_FALSE, 8 * sizeof(float), 0);
+    aPos.enableVertexAttribArray(); // position attribute
+    aColor.vertexAttribPointer(gl::VertexAttribSize::Triple, gl::VertexAttribType::Float, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float));
+    aColor.enableVertexAttribArray(); // color attribute
+    aTexCoord.vertexAttribPointer(gl::VertexAttribSize::Double, gl::VertexAttribType::Float, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float));
+    aTexCoord.enableVertexAttribArray(); // texture coord attribute
+
+    gl::Texture2D texture1, texture2; // create textures
+    StbImage::setFlipVerticallyOnLoad(true); // tell stb_image library to flip loaded texture's on the y-axis
+    StbImage image1(IDI_CONTAINER), image2(IDI_AWESOMEFACE); // load images
+    texture1.textureWrapS = gl::TextureWrapMode::ClampToEdge;  // note that we set the container wrapping method to GL_CLAMP_TO_EDGE
+    texture1.textureWrapT = gl::TextureWrapMode::ClampToEdge;
+    texture1.textureMinFilter = gl::TextureMinFilter::Linear; // set texture filtering parameters
+    texture1.textureMagFilter = gl::TextureMagFilter::Linear;
+    texture1.texImage(image1, 0); // take the format and type from StbImage object
+    texture1.generateMipmap();
+
+    texture2.textureWrapS = gl::TextureWrapMode::Repeat; // we want to repeat the awesomeface pattern so we kept it at GL_REPEAT
+    texture2.textureWrapT = gl::TextureWrapMode::Repeat;
+    texture2.textureMinFilter = gl::TextureMinFilter::Linear; // set texture filtering parameters
+    texture2.textureMagFilter = gl::TextureMagFilter::Linear;
+    texture2.texImage(image2, 0); // take the format and type from StbImage object
+    texture2.generateMipmap();
+
+    ourShader.useProgram(); // activate shader
+    ourShader.setInt("texture1", 0); // set 0 to sampler2D uniform
+    ourShader.getUniformLocation("texture2").uniformTextureUnit(1); // same thing using uniformTextureUnit
+
+    while (!window.windowShouldClose) // render loop
+    {
+        gl::clearColor(0.2f, 0.3f, 0.3f, 1.0f); // render
+        gl::clear(gl::BufferBitMask::ColorBufferBit);
+
+        texture1.activeTexture(0); // bind textures on corresponding texture units
+        texture2.activeTexture(1);
+        ourShader.useProgram(); // render container and awesome face
+        VAO.bindVertexArray();
+        gl::drawElements(gl::PrimitiveMode::Triangles, 6, gl::PrimitiveType::UnsignedInt, 0);
+
+        window.swapBuffers(); // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfw::pollEvents();
+    }
+    return 0;
+}
