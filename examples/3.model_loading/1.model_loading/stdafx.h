0302d345dae114aee6371cd9b8cee5018bed46d6
dmitrycello <156050563+dmitrycello@users.noreply.github.com>
2024-11-06 01:30:01 -0500

    Add files via upload

---
+++ examples/3.model_loading/1.model_loading/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h
+#pragma once
+
+// Windows API
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+// YAGL++ header
+#include "config.h"
+#include <yaglpp/glpp.h>
