ed6aabe4ad97ec0b674e1d992e1f7e3be9a6343e
dmitrycello <156050563+dmitrycello@users.noreply.github.com>
2024-11-06 01:28:42 -0500

    Add files via upload

---
+++ examples/3.model_loading/1.model_loading/helper.h
@@ -0,0 +1,45 @@
+// helper.h
+#pragma once
+/*LearnOpenGL: utility function for loading a 2D texture from resource id*/
+void load_texture(gl::Texture2D& texture, int rcid)
+{
+    StbImage image(rcid);
+    texture.texImage(image, 0);
+    texture.generateMipmap();
+    texture.textureWrapS = gl::TextureWrapMode::Repeat;
+    texture.textureWrapT = gl::TextureWrapMode::Repeat;
+    texture.textureMagFilter = gl::TextureMagFilter::Linear;
+    texture.textureMinFilter = gl::TextureMinFilter::LinearMipmapLinear;
+}
+/*LearnOpenGL: utility function for loading a 2D texture from file*/
+void load_texture_file(gl::Texture2D& texture, const char* path)
+{
+    StbImage image(path);
+    texture.texImage(image, 0);
+    texture.generateMipmap();
+    texture.textureWrapS = gl::TextureWrapMode::Repeat;
+    texture.textureWrapT = gl::TextureWrapMode::Repeat;
+    texture.textureMagFilter = gl::TextureMagFilter::Linear;
+    texture.textureMinFilter = gl::TextureMinFilter::LinearMipmapLinear;
+}
+/*LearnOpenGL: utility function showing a dialog box to retreive a file name and path*/
+bool open_file(std::string& path, LPCSTR lpstrTitle, LPCSTR lpstrFilter)
+{
+    OPENFILENAMEA ofn;
+    char buffer[MAX_PATH];
+    memset(&ofn, 0, sizeof(OPENFILENAMEA));
+    buffer[0] = '\0';
+    ofn.lStructSize = sizeof(OPENFILENAMEA);
+    ofn.lpstrFilter = lpstrFilter;
+    ofn.lpstrFile = buffer;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrTitle = lpstrTitle;
+    ofn.Flags = OFN_ENABLESIZING | OFN_FILEMUSTEXIST;
+    ofn.nFileOffset = 1;
+    if (GetOpenFileNameA(&ofn))
+    {
+        path.assign(buffer, MAX_PATH);
+        return true;
+    }
+    else return false;
+}
---
+++ examples/3.model_loading/1.model_loading/model_loading.cpp
@@ -0,0 +1,54 @@
+// model_loading.cpp
+#include "stdafx.h"
+#include "helper.h"
+#include "camera.h"
+#include "window.h"
+#include "shader.h"
+#include "mesh.h"
+#include "model.h"
+#include "resource.h"
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+//Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+int main()
+{
+    std::string path;
+    if (!open_file(path, "Select 3D Model Object File", "Object Files (*.obj)\0*.obj\0All Files (*.*)\0*.*\0\0"))
+    {
+        return -1; // no file selected
+    }
+    Window window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL"); // glfw window creation
+    window.cursorMode = glfw::CursorMode::CursorDisabled; // tell GLFW to capture our mouse
+    window.makeContextCurrent();
+    gl::enableDepthTest(); // configure global opengl state
+    StbImage::setFlipVerticallyOnLoad(true); // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    Shader ourShader(IDT_VERT_SHADER, IDT_FRAG_SHADER); // build and compile shaders
+    Model ourModel(path); // load models
+    //gl::polygonMode(gl::PolygonMode::Line); // draw in wireframe
+
+    while (!window.windowShouldClose) // render loop
+    {
+        window.processInput();
+        gl::clearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        gl::clear(gl::BufferBitMask::ColorBufferBit | gl::BufferBitMask::DepthBufferBit);
+        ourShader.useProgram(); // don't forget to enable shader before setting uniforms
+
+        // view/projection transformations
+        glm::mat4 projection = window.camera.getProjectionMatrix(window.getAspectRatio());
+        glm::mat4 view = window.camera.getViewMatrix();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+
+        // render the loaded model
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        ourShader.setMat4("model", model);
+        ourModel.Draw(ourShader);
+
+        window.swapBuffers(); // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfw::pollEvents();
+    }
+    return 0;
+}
